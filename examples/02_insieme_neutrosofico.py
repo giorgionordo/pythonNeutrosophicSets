2b03c39ad11df6f903512a18e1c34124c6916f69
giorgionordo <34978583+giorgionordo@users.noreply.github.com>
2023-07-10 00:51:25 +0200

    Add files via upload

--- examples/02_insieme_neutrosofico.py
+++ examples/02_insieme_neutrosofico.py
@@ -5,7 +5,7 @@ from pyns.ns_set import NSset
 U = NSuniverse(1,2,3)
 A = NSset(U)
 A.setElement(1,(0.7,0.3,0.1))
-A.setElement(2,(0.4,0.6,0.9))
+A.setElement(2,"0.4,0.6,0.9")
 A.setMembership(3, 1)
 A.setIndeterminacy(3, 0.2)
 print(A)
